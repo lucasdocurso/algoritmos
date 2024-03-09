1dbc34605e9b8d8614f0eb744e8c29f4fa5148bb
lucasdocurso <162522619+lucasdocurso@users.noreply.github.com>
2024-03-08 21:57:52 -0300

    Add files via upload

---
+++ aula3/exemplo.js
@@ -0,0 +1,27 @@
+//este e um comentario de uma linha única.
+//este comando imprime algo no terminal(console.log)
+//instalar o app node no meu notebbok de casa.
+//const = nova variavel.   nomes nao podem comecar com numeros e caracteres especiais
+//let + variavel que pode alterar o valor.   nomes nao podem comecar com numeros e caracteres especiais
+//primeira letra da variavel sempre minuscula e se for composta e maiuscula.
+//SRINGS AND NUMBERS(ABAIXO)
+/*nunbers: São os tipos que representam números
+const idade = 23
+const altura = 1.90
+const temperatura = -20
+Strings: são os tipos que representam conjunto de caracteres(texto)
+const nome = "lucas"
+let idade = "23"*/
+/*TIPOS-BOOLEANS(ABAIXO)
+VARIAVEIS BOOLEANAS:são variaveis que só assumem os valores TRUE or FALSE
+let: souUmBomBoolean = true
+souUmBomBoolean = false*/
+const primeironome = "Lucas"
+const sobrenome = "Florao"
+const idade = "16"
+const souUmBomBoolean = true
+console.log("Olá,", "meu nome é", primeironome, sobrenome, "eu tenho", 16, "anos de idade","sou estudante?", souUmBomBoolean)
+const serie = "you"
+const temporada = 4
+console.log(typeof serie) //string
+console.log(typeof temporadas) //number
---
+++ aula3/exemplo2.js
@@ -0,0 +1,3 @@
+const numero1 = '5'
+const numero2 = '10'
+console.log(numero1 + numero2)
